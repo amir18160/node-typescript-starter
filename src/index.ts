c9cbb7cf29297f02d0bcc9cbdfcb6c1574384c42
Amir Hoori <amirhosseinhoori@gmail.com>
2025-03-06 15:38:28 +0330

    add nodemon settings

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-/* eslint-disable no-console */
-console.log("Hello, World!");
-console.log("This is NodeJS Starter Project!");
+import { createInterface } from "readline";
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+async function ask(question: string): Promise<string> {
+    return new Promise((resolve) => {
+        rl.question(question, resolve);
+    });
+}
+
+(async function runHello() {
+    await ask("What is your name? ");
+    rl.close();
+})();
